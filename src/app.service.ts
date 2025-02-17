a287351129e286c840b115b8df0ce305508ee2b4
Dmitri Lishko <dmitrilishko@gmail.com>
2025-02-16 22:36:57 -0700

    feat: add app controller

---
+++ src/app.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { ClientProxy, ClientProxyFactory } from '@nestjs/microservices';
+
+@Injectable()
+export class AppService {
+  private client: ClientProxy;
+  constructor() {
+    this.client = ClientProxyFactory.create({
+      transport: Transport.TCP,
+      options: {
+        port: 8877,
+        host: 'localhost',
+      },
+    });
+  }
+  sendMessage() {
+    this.client.emit('message', 'New order');
+  }
+}
